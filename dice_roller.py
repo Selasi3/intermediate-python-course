949f3b62cb81b832fcbf362ae9c3a9fcff6419b4
Selasi Ayittah <selasiayittah3@gmail.com>
2021-10-13 07:11:50 +0000

    flexible die roller

--- dice_roller.py
+++ dice_roller.py
@@ -2,12 +2,13 @@ import random
 
 def main():
   
-  dice_rolls =2
+  dice_rolls =int(input("How many dice would you like to roll?\n"))
+  dice_size = int(input("How many sides are the dice?\n"))
   dice_sum = 0
 
   for i in range(0, dice_rolls):
     #Generating a random number between 1 and 6
-    roll = random.randint(1,6)
+    roll = random.randint(1,dice_size)
     dice_sum+=roll
     if (roll==1):
       print(f"You rolled a {roll}! Critical Fail")
